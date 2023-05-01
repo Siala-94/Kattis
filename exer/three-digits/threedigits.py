6cdc7b413e522d679263c2abb55612f3097ca831
Hassan Mustafa <hassanmustafa@Hassans-MacBook-Pro.local>
2023-05-01 10:46:43 +0200

    done

--- exer/three-digits/threedigits.py
+++ exer/three-digits/threedigits.py
@@ -1,11 +1,11 @@
 import sys
 number = int(sys.stdin.readline())
-result = 1
 
+result = 1
 for i in range(2, number+1):
     result *= i
 
-    while result % 10 == 0 and result != 0:
+    while result % 10 == 0:
         result //= 10
 
     result %= 10**12
